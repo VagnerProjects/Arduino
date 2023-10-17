c48387638a4e67d75f00eab38eee22c64b853735
Vagner <55877172+VagnerProjects@users.noreply.github.com>
2023-10-17 11:40:29 -0300

    Update music.cpp

--- music.cpp
+++ music.cpp
@@ -1,14 +1,1 @@
-#include "music.h"
-#include <Arduino.h>
-
-Music::Music(int tempo, int compasso)
-{
-	this->tempo = tempo;
-	this->compasso = compasso;
-	this->divisor = 0;
-	this->duracao = 0;
-
-	this->semibreve = (60000 * compasso) / this->tempo;	
-}
-
 
